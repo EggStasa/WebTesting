d55d7c419727c6fa3da20a62573d4d5c30425066
EggStasa <94902411+EggStasa@users.noreply.github.com>
2025-02-24 12:25:33 +0200

    Delete webTesting directory

--- webTesting/src/main/java/KRCPK/WebTesting/repo/TestsRepository.java
+++
@@ -1,8 +0,0 @@
-package KRCPK.WebTesting.repo;
-
-import KRCPK.WebTesting.model.Tests;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestsRepository extends JpaRepository<Tests, Long> {
-
-}
